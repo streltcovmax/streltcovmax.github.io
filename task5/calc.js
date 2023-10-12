f42c3ab5a3d48802bdd4e8dc92f94c62b60cfa07
streltcovmax <144123001+streltcovmax@users.noreply.github.com>
2023-10-12 19:53:25 +0300

    Add files via upload

---
+++ task5/calc.js
@@ -0,0 +1,45 @@
+function calculatePrice() {
+    let selectedProduct = document.getElementById("selection").value;
+    let amount = document.getElementById("amount").value;
+    let isNumber = /^[0-9]*(\.[0-9]+)?$/.test(amount);
+    let price = getProductPrice(selectedProduct);
+  
+    if (isNumber) 
+    {
+      let parsedAmount = parseFloat(amount);
+      if (Number.isInteger(parsedAmount)) 
+      {
+        let сost = price * parsedAmount;
+        document.getElementById("result").textContent = "ИТОГО: " + сost + " руб";
+      }
+    } 
+    else 
+    {
+      document.getElementById("result").textContent =
+        "Кол-во товара введено некорректно";
+    }
+  }
+  
+  function getProductPrice(productName) {
+    switch (productName) {
+      case "Товар1":
+        return 100;
+      case "Товар2":
+        return 50;
+      case "Товар3":
+        return 129;
+      case "Товар4":
+        return 49;
+      case "Товар5":
+        return 69;
+      case "Товар6":
+        return 115;
+      case "Товар7":
+        return 100;
+      case "Товар8":
+        return 70;
+  
+      default:
+        return 0;
+    }
+  }
