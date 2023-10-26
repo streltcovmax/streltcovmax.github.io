3c6effeabeebb9da6bb6ed4d6a359afbfc671005
streltcovmax <144123001+streltcovmax@users.noreply.github.com>
2023-10-27 01:38:16 +0300

    Update script.js

--- task7/script.js
+++ task7/script.js
@@ -1,8 +1,24 @@
 window.onload = function () {
     $('.gallery').slick({
         infinite: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 620,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+            },
+            {
+              breakpoint: 800,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            }
+        ],
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true
     });
 };
