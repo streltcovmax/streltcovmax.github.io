9bc13af205babe42f17cbb49019381a06cc77058
streltcovmax <144123001+streltcovmax@users.noreply.github.com>
2023-11-28 00:32:39 +0300

    Add files via upload

--- task8/script.js
+++ task8/script.js
@@ -1,42 +1,48 @@
-$(function () {
-  $(".formcarryForm").submit(function (e) {
-    e.preventDefault();
-    var form = $(this);
-    var href = form.attr("action");
+ const messageBox = document.getElementById("messageBox");
 
-    $.ajax({
-      type: "POST",
-      url: href,
-      data: new FormData(this),
-      dataType: "json",
-      processData: false,
-      contentType: false,
-      success: function (response) {
-        if (response.code === 200) {
-          alert("Успешно отправлено");
+      $(function () {
+        $(".formcarryForm").submit(function (e) {
+          e.preventDefault();
+          // REQUIRED FOR RESET
+          var form = $(this);
+          var href = form.attr("action");
 
-          form[0].reset();
-        } else {
-          alert("Ошибка: " + response.message);
-        }
-      },
-      error: function (err, textStatus) {
-        const error = err.responseJSON;
+          $.ajax({
+            type: "POST",
+            url: href,
+            data: new FormData(this),
+            dataType: "json",
+            processData: false,
+            contentType: false,
+            success: function (response) {
+              if (response.code === 200) {
+                alert("We received your submission, thank you!");
 
-        // validation error
-        $.each(error.errors, function (key) {
-          var field = $('[name="' + key + '"]');
-          field.addClass("fc-field-error");
+                // REQUIRED FOR RESET
+                // Reset your form fields after a successful submission
+                // If you want to reset fields whether request is successful or not, move this piece of code into complete function
+                form[0].reset();
+              } else {
+                alert("An error occured: " + response.message);
+              }
+            },
+            error: function (err, textStatus) {
+              const error = err.responseJSON;
+
+              // validation error
+              $.each(error.errors, function (key) {
+                var field = $('[name="' + key + '"]');
+                field.addClass("fc-field-error");
+              });
+
+              alert(
+                "Request failed, " +
+                  errorObject.title +
+                  ": " +
+                  errorObject.message
+              );
+            },
+            complete: function () {},
+          });
         });
-
-        alert(
-          "Request failed, " +
-            errorObject.title +
-            ": " +
-            errorObject.message
-        );
-      },
-      complete: function () {},
-    });
-  });
-});
+      });
